5ae6aad6836a4684cbbb7b2bbb84b1e3e9972c4d
Steven Douglas <elstevi@gmail.com>
2026-03-18 14:09:51 -0700

    correct home var

--- .config/yadm/install.sh
+++ .config/yadm/install.sh
@@ -2,6 +2,6 @@
 
 # curl https://raw.githubusercontent.com/elstevi/dotfiles/refs/heads/master/.config/yadm/install.sh | bash
 # Install yadm, clearing old installs out
-rm -r ${home}/.local/share/yadm ${home}/.config/yadm
+rm -r ${HOME}/.local/share/yadm ${HOME}/.config/yadm
 yadm clone -f --bootstrap https://github.com/elstevi/dotfiles
 yadm reset --hard origin/master
