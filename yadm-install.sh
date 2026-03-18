421ef5eeaf05d53f4497db832471afc926f72440
Steven Douglas <elstevi@gmail.com>
2026-03-18 12:17:00 -0700

    add comments to installer

--- yadm-install.sh
+++ yadm-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Install yadm, clearing old installs out
 rm -r ${home}/.local/share/yadm ${home}/.config/yadm
 yadm clone -f --bootstrap https://github.com/elstevi/dotfiles
 yadm reset --hard origin/master
