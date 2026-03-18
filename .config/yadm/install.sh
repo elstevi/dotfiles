56733c1dd33f1b5e782ae489e10186bdeb7e49ef
Steven Douglas <elstevi@gmail.com>
2026-03-18 14:06:59 -0700

    move install script and add example invocation

--- .config/yadm/install.sh
+++ .config/yadm/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# curl https://raw.githubusercontent.com/elstevi/dotfiles/refs/heads/master/.config/yadm/install.sh | bash
 # Install yadm, clearing old installs out
 rm -r ${home}/.local/share/yadm ${home}/.config/yadm
 yadm clone -f --bootstrap https://github.com/elstevi/dotfiles
